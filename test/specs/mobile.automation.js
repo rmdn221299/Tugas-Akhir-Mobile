5a4cbd2916223079e3e556a2ed2d60659e9c6fda
rmdn221299 <ramm.dani221299@yahoo.com>
2024-05-20 14:47:52 +0700

    commit lagi walopun pusing

--- test/specs/mobile.automation.js
+++ test/specs/mobile.automation.js
@@ -1,4 +1,17 @@
 describe('Open Application',() =>{
     it('should open apps', async() => {
+        await $("com.fghilmany.dietmealapp:id/et_name").click();
+        await $("com.fghilmany.dietmealapp:id/et_name").sendKeys("ramdani");
+        await $("com.fghilmany.dietmealapp:id/et_weight").clicks();
+        await $("com.fghilmany.dietmealapp:id/et_weight").sendKeys("123123");
+        await $("com.fghilmany.dietmealapp:id/et_height").click();
+        await $("com.fghilmany.dietmealapp:id/et_weight").sendKeys("123123");
+        await $("com.fghilmany.dietmealapp:id/rb_male").click();
+        await $("com.fghilmany.dietmealapp:id/bt_next").click();
+        await $("com.fghilmany.dietmealapp:id/bt_rest").click();
+        await $("com.fghilmany.dietmealapp:id/bt_finish").click();
+
+      
+
     })
 })
