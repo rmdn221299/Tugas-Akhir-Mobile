d79fec57a5712f547d4f3679de603e40ef0db435
rmdn221299 <ramm.dani221299@yahoo.com>
2024-05-21 11:05:40 +0700

    commit lagi walopun pusing

--- test/specs/mobile.automation.js
+++ test/specs/mobile.automation.js
@@ -1,14 +1,12 @@
-describe('Open Application',() =>{
-    it('should open apps', async() => {
-        await $("com.fghilmany.dietmealapp:id/et_name").click();
-        await $("com.fghilmany.dietmealapp:id/et_name").sendKeys("ramdani");
-        await $("com.fghilmany.dietmealapp:id/et_weight").clicks();
-        await $("com.fghilmany.dietmealapp:id/et_weight").sendKeys("123123");
-        await $("com.fghilmany.dietmealapp:id/et_height").click();
-        await $("com.fghilmany.dietmealapp:id/et_weight").sendKeys("123123");
-        await $("com.fghilmany.dietmealapp:id/rb_male").click();
-        await $("com.fghilmany.dietmealapp:id/bt_next").click();
-        await $("com.fghilmany.dietmealapp:id/bt_rest").click();
-        await $("com.fghilmany.dietmealapp:id/bt_finish").click();
-       })
+
+describe('My login application', () => {
+    it ('should login with valid cridentials', async () =>{
+        await $('id:com.fghilmany.dietmealapp:id/et_name').setValue('ramdani');
+        await $('id:com.fghilmany.dietmealapp:id/et_weight').setValue('123');
+        await $('id:com.fghilmany.dietmealapp:id/et_height').setValue('123');
+        await $('id:com.fghilmany.dietmealapp:id/rb_male').click();
+        await $('id:com.fghilmany.dietmealapp:id/bt_next').click();
+        await $('id:com.fghilmany.dietmealapp:id/rb_rest').click();
+        await $('id:com.fghilmany.dietmealapp:id/bt_finish').click();
+    })
 })
